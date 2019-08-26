fa6e4be3fcb882e2cd26b41010a5a4b34f3aaa13
Alex Jabbour <redpod22@gmail.com>
2019-08-26 14:17:33 +0300

    Humble yourself and you will be exalted

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+
+export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
