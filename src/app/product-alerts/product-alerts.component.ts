f7d09431f533cf2f881899ea4eeeaf569e83fed2
Alex Jabbour <redpod22@gmail.com>
2019-08-27 12:41:13 +0300

    Success is the strongest drug known to man

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,6 +10,8 @@ import { Input } from '@angular/core';
 
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
 
 
